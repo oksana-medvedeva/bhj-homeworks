b28ed6f612ba73211c7f76b615feb961e3473fac
Oksana Staeglazova <oksanik.med@gmail.com>
2021-11-22 20:33:53 +0300

    task keysolo completed

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -24,6 +24,16 @@ class Game {
       В случае правильного ввода слова вызываем this.success()
       При неправильном вводе символа - this.fail();
      */
+     document.addEventListener('keyup', e => {
+      if (!e.code.startsWith("Key")) {
+        return true;
+      }
+      if (e.key.toLowerCase() === this.currentSymbol.textContent.toLowerCase()) {
+          this.success();
+        } else {
+          this.fail();
+        }
+     })
   }
 
   success() {
